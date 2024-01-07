a4a41d109f22952e0f8e4038540dbc3a51c5baa5
Andrie-E <stpaul.martinla@gmail.com>
2024-01-07 16:21:47 +0800

    all input commands

--- if-else-statements.py
+++ if-else-statements.py
@@ -3,8 +3,13 @@
 # pseudocode
 
 # Code that ask the users to input 5 numbers
-first_number = float(input("Please enter your first number here:"))
-print(first_number)
+first_number = float(input("Please enter your first number here: "))
+second_number = float(input("Please enter your second number here: "))
+third_number = float(input("Please enter your third number here: "))
+fourth_number = float(input("Please enter your fourth number here: "))
+fifth_number = float(input("Please enter your fifth number here: "))
+
+
 # Code that finds the biggest number out of all the inputed numbers
 
 # Code that prints the largest number
