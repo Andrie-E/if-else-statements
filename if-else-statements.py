2fb520d673450463f107c04a69a6dad29bb06c42
Andrie-E <stpaul.martinla@gmail.com>
2024-01-07 16:28:06 +0800

    first if statement

--- if-else-statements.py
+++ if-else-statements.py
@@ -8,8 +8,8 @@ second_number = float(input("Please enter your second number here: "))
 third_number = float(input("Please enter your third number here: "))
 fourth_number = float(input("Please enter your fourth number here: "))
 fifth_number = float(input("Please enter your fifth number here: "))
-
-
 # Code that finds the biggest number out of all the inputed numbers
-
+if first_number >= second_number and first_number >= third_number and first_number >= fourth_number and first_number >= fifth_number:
+    largest_number= first_number
+print(largest_number)
 # Code that prints the largest number
