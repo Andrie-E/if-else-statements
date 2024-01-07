2d1360a2b7cf8b90c8bf742e5d21764909a18907
Andrie-E <stpaul.martinla@gmail.com>
2024-01-07 16:34:42 +0800

    elif statement for second input

--- if-else-statements.py
+++ if-else-statements.py
@@ -11,5 +11,7 @@ fifth_number = float(input("Please enter your fifth number here: "))
 # Code that finds the biggest number out of all the inputed numbers
 if first_number >= second_number and first_number >= third_number and first_number >= fourth_number and first_number >= fifth_number:
     largest_number= first_number
+elif second_number >= first_number and second_number >= third_number and second_number >= fourth_number and second_number >= fifth_number:
+    largest_number= second_number
 print(largest_number)
 # Code that prints the largest number
