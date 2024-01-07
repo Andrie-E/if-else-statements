5e5fa337a21c9c100328efb82617f0cbf16b1778
Andrie-E <stpaul.martinla@gmail.com>
2024-01-07 17:17:54 +0800

    error message if the user enters a word or letter instead of a number

--- if-else-statements.py
+++ if-else-statements.py
@@ -1,13 +1,20 @@
 # Ask user to input 5 numbers. Find and print the biggest number 
 
 # pseudocode
-
+# An error warning to write numbers only if the user entered a word or letter
+def real_number(prompt):
+    while True:
+        try:
+            num = float(input(prompt))
+            return num
+        except ValueError:
+            print("Please enter a number and not a word!")
 # Code that ask the users to input 5 numbers
-first_number = float(input("Please enter your first number here: "))
-second_number = float(input("Please enter your second number here: "))
-third_number = float(input("Please enter your third number here: "))
-fourth_number = float(input("Please enter your fourth number here: "))
-fifth_number = float(input("Please enter your fifth number here: "))
+first_number = real_number("Please enter your first number here: ")
+second_number = real_number("Please enter your second number here: ")
+third_number = real_number("Please enter your third number here: ")
+fourth_number = real_number("Please enter your fourth number here: ")
+fifth_number = real_number("Please enter your fifth number here: ")
 # Code that finds the biggest number out of all the inputed numbers
 if first_number >= second_number and first_number >= third_number and first_number >= fourth_number and first_number >= fifth_number:
     largest_number= first_number
@@ -22,3 +29,5 @@ else:
 
 # Code that prints the largest number
 print("Base on the data that you have provided, the largest number is: ", largest_number)
+
+# Error message if the user enters a word or letter
