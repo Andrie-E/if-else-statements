9fc0b74771b901a1b640e2ef9a8aa95016542b98
Andrie-E <stpaul.martinla@gmail.com>
2024-01-07 16:16:40 +0800

    first number

---
+++ if-else-statements.py
@@ -0,0 +1,10 @@
+# Ask user to input 5 numbers. Find and print the biggest number 
+
+# pseudocode
+
+# Code that ask the users to input 5 numbers
+first_number = float(input("Please enter your first number here:"))
+print(first_number)
+# Code that finds the biggest number out of all the inputed numbers
+
+# Code that prints the largest number
